d85df5b61bd3b35e59c5e5a9633f8cc9ab390b23
Darbaz Ali <darbaz.me@gmail.com>
2021-05-15 05:29:05 +0300

    01 - Two Number Sum - first solution - revisit

--- Arrays/01_E_twoNumberSum.js
+++ Arrays/01_E_twoNumberSum.js
@@ -7,21 +7,22 @@ Time = O(n^2)
 Space = O(1)
 =================================================================================*/
 const twoNumberSum = (array, targetSum) => {
-  // travers the array untill before last index
-  for (let i = 0; i < array.length - 1; i++) {
-    const firstNum = array[i]
-    for (let j = i + 1; j < array.length; j++) {
-      const secondNum = array[j]
-
-      //   if [x + y] = target => reutrn [x, y]
-      if (firstNum + secondNum === targetSum) {
-        return [firstNum, secondNum]
+  // travers the array untill before last element
+  const len = array.length
+  for (let i = 0; i < len - 1; i++) {
+    const current = array[i]
+    for (let j = i + 1; j < len; j++) {
+      const next = array[j]
+      if (current + next === targetSum) {
+        return [current, next]
       }
     }
   }
 
   return []
 }
+
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
 
 /*================================================================================
 Second Solution - Using Hash Tables
@@ -78,6 +79,5 @@ const twoNumberSum3 = (array, targetSum) => {
   return []
 }
 
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
 console.log(twoNumberSum2([3, 5, -4, 8, 11, 1, -1, 6], 10))
 console.log(twoNumberSum3([3, 5, -4, 8, 11, 1, -1, 6], 10))
