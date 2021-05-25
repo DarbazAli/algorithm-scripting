d0ba918e6e51b7b2425104f077f78b783aae46de
Darbaz Ali <darbaz.me@gmail.com>
2021-05-25 23:41:16 +0300

    refactor isMonotonic

--- Arrays/isMonotonic.js
+++ Arrays/isMonotonic.js
@@ -2,24 +2,27 @@
 
 /* 
     Is Monotonic array
-
     a monotonic is from left to right ether entierly non-increasing or entierly non-decreasing
-
-
 */
 
+// O(n) Time | O(1) Space
 const isMonotonic = (array) => {
   let isNonDecreasing = true
   let isNonIncreasing = true
 
   for (let i = 1; i < array.length; i++) {
-    if (array[i] < array[i - 1]) isNonIncreasing = false
-    if (array[i] > array[i - 1]) isNonDecreasing = false
+    const prev = array[i - 1]
+    const current = array[i]
+
+    if (current < prev) isNonDecreasing = false
+    if (current > prev) isNonIncreasing = false
   }
 
-  return isNonIncreasing || isNonDecreasing
+  return isNonDecreasing || isNonIncreasing
 }
 
 const arr = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
 
 console.log(isMonotonic(arr)) // should return true
+console.log(isMonotonic([1, 1, 1, 2])) // should return true
+console.log(isMonotonic([1, 1, 1, 2, 3, 2])) // should return false
