ce23d8a161978f315b8edf802e0fd051737334d5
Darbaz Ali <darbaz.me@gmail.com>
2022-01-01 23:34:54 +0300

    basic stack

---
+++ Stacks/basicStack.js
@@ -0,0 +1,8 @@
+// stack data structure
+var stackArr = ["BIO12", "HIS80", "MAT122", "PSY44"]
+
+// remove from top using pop() method
+stackArr.pop()
+
+// add to the top using push() method
+stackArr.push("CS50")
